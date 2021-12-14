264580863eb23e2917fb35a8cf79ea013f0d94f3
RicardoLee <33721335+RicardoMLi@users.noreply.github.com>
2021-12-14 10:17:03 +0800

    Update models.py
    add BoTNet_with_classifier in model.py

--- models.py
+++ models.py
@@ -438,6 +438,54 @@ class BoTNet(nn.Module):
 
         return out
 
+    
+class BoTNet_with_classifier(nn.Module):
+    def __init__(self, block, num_blocks, resolution=(14, 14), heads=4, num_classes=1):
+        super(BoTNet_with_classifier, self).__init__()
+        self.in_planes = 32
+        self.resolution = list(resolution)
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=2, heads=heads, mhsa=True)
+
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.avgpool = nn.Flatten()
+        self.fc = nn.Linear(512*4, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride=1, heads=4, mhsa=False):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for idx, stride in enumerate(strides):
+            layers.append(block(self.in_planes, planes, stride, heads, mhsa, self.resolution))
+            if stride == 2:
+                self.resolution[0] = math.ceil(self.resolution[0]/2)
+                self.resolution[1] = math.ceil(self.resolution[1]/2)
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        if self.num_classes == 1:
+            return nn.Sigmoid()(out)
+
+        return out    
+   
 
 class DepthSeperabelConv2d(nn.Module):
 
